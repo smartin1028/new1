e97935b502137babb58970186fa8f49d897657ab
DESKTOP-H5KNGP2\ldw <leedw3232@gmail.com>
2022-04-25 23:25:38 +0900

    1111

--- src/main/java/com/hello/HelloApplication.java
+++ src/main/java/com/hello/HelloApplication.java
@@ -9,6 +9,7 @@ public class HelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 		System.out.println("test 1aaaa018 ");
+		System.out.println("할머니 바보!!");
 	}
 
 }
