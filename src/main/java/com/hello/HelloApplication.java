5a5432ee20d8fb100421e987b037b67a71901253
DESKTOP-H5KNGP2\ldw <leedw3232@gmail.com>
2022-04-25 23:09:36 +0900

    11

--- src/main/java/com/hello/HelloApplication.java
+++ src/main/java/com/hello/HelloApplication.java
@@ -8,7 +8,7 @@ public class HelloApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
-		System.out.println("test 111111 ");
+		System.out.println("test 1018 ");
 	}
 
 }
